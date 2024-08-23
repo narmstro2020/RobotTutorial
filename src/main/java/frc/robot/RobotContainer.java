2344f93da0a20cb5b686af4487655fd2e2f8d3c6
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-08-23 12:23:24 -0400

    done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,13 +8,31 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
+
+  /*
+   * This is where we define our robot components and/or subsystems in the properties/fields area
+   */
+
   public RobotContainer() {
+
+    /*
+     * Do other initialization stuff
+     */
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    /*
+     * Connect buttons and other triggers to commands
+     */
+  }
 
   public Command getAutonomousCommand() {
+    /*
+     * Acquire the command currently selected and pass it up to autoInit in Robot.  
+     */
     return Commands.print("No autonomous command configured");
   }
 }
