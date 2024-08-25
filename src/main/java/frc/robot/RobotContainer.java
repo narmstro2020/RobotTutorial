281ba34735e80e8e1900f5c45bb5b8dc267c61f1
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-08-25 11:55:23 -0400

    done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,34 +4,48 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
 
-  /*
-   * This is where we define our robot components and/or subsystems in the properties/fields area
-   */
+  private final PneumaticHub pneumaticHub = new PneumaticHub();
+  private final Compressor pneumaticHubCompressor = pneumaticHub.makeCompressor();
+  private final Solenoid solenoidRev = pneumaticHub.makeSolenoid(0);
+  private final Solenoid solenoidCTRE = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+  private final CommandXboxController controller = new CommandXboxController(0);
 
   public RobotContainer() {
-
-    /*
-     * Do other initialization stuff
-     */
-
+    pneumaticHub.enableCompressorDigital();
+    SmartDashboard.putData(solenoidRev);
+    SmartDashboard.putData(pneumaticHubCompressor);
     configureBindings();
   }
 
   private void configureBindings() {
+    Trigger aTrigger = controller.a();
+    Runnable revOnAction = () -> solenoidRev.set(true);  
+    Command revOnCommand = Commands.runOnce(revOnAction);
+    Runnable revOffAction = () -> solenoidRev.set(false);
+    Command revOffCommand = Commands.runOnce(revOffAction);
+    aTrigger.onTrue(revOnCommand);
+    aTrigger.onFalse(revOffCommand);
 
-    /*
-     * Connect buttons and other triggers to commands
-     */
+    // leave the ctre solenoid as an exercise for you
+    // use the b button.
+
   }
 
   public Command getAutonomousCommand() {
     /*
-     * Acquire the command currently selected and pass it up to autoInit in Robot.  
+     * Acquire the command currently selected and pass it up to autoInit in Robot.
      */
     return Commands.print("No autonomous command configured");
   }
